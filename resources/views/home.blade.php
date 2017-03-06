681816e2c689293954657ede7ace22861e0734c4
Hemnys <hemnys25@gmail.com>
2017-03-05 20:18:58 -0400

    Creando panel de Post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,18 @@
                 <div class="panel-heading">Bienvenido al grupo {{$NameGroup}}</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <div class="row">
+                        <div class="col-md-3">
+                            
+                            <a href="" class="btn btn-primary">Crear Nuevo Post</a>
+                        </div>
+                        <div class="col-md-9">
+                            
+                            <select name="Categories" id="" class="form-control">
+                                <option value="">Elegir Categoria para ver post:</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
